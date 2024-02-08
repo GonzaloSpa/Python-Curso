2588a3c6adb7adf31350939f9212df1c5957eac4
GonzaloSpa <gonzalospaltro98@gmail.com>
2024-02-08 15:01:28 -0300

    primer hola mundo con FastAPI

---
+++ Backend/FastAPI/main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")        ##entrando al contexto de fastApi con el @app
+async def root():
+    return "¡Hola FastAPI!"
+
+
+@app.get("/url")        
+async def url():
+    return {"url_curso" : "https://mouredev.com/python"}
