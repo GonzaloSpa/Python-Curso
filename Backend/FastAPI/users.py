007d5f3844819fd32810d7e97f995aff3ef9ca01
GonzaloSpa <gonzalospaltro98@gmail.com>
2024-02-16 04:11:35 -0300

    metodo get, post, put, delete

--- Backend/FastAPI/users.py
+++ Backend/FastAPI/users.py
@@ -36,9 +36,53 @@ async def user(id: int):
     return search_users(id)
 # http://127.0.0.1:8000/user/?id=1 el id="int" lleva el valor de la id del user  
 
+
+
+# crendo un POST
+
+@app.post("/user/")
+async def user(user: User):
+    if type(search_users(user.id)) == User:
+        return {"error": "El usuario ya existe"}
+    else:
+        users_list.append(user)
+        return user
+    
+
+# creando un PUT (actualiza)
+
+@app.put("/user/")
+async def user(user: User):
+    found = False
+
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == user.id:
+            users_list[index] = user
+            found = True
+    if not found:
+        return {"error": "No se ha podido actualizar el usuario"}    
+    else:      
+        return user  
+    
+ 
+# creando un DELETE
+
+@app.delete("/user/{id}")        # por PATH el id es obligatorio
+async def user(id: int):
+    found = False
+    for index, saved_user in enumerate(users_list):
+        if saved_user.id == id:
+           del users_list[index]
+           found =True           
+    
+    if not found:
+        return {"error": "No se ha podido eliminar el usuario"}      
+ 
+ 
 def search_users(id: int):
     users = filter(lambda user: user.id == id, users_list)
     try:
         return list(users)[0]
     except:
         return {"error": "No se ha podido encontrar el usuario"}
+    
